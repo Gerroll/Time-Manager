4197836611fe18d769ef9297acd545fc83be2e87
Loic <loic.roger@epitech.eu>
2019-09-20 10:55:08 +0200

    push routes users

---
+++ front/src/components/ReportTIme.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Report Time</h1>
+</template>
+
+<script>
+    export default {
+        name: "ReportTIme"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -4,6 +4,8 @@ import Home from '@/components/Home'
 import Users from '@/components/Users'
 import General_Manager from "../components/General_Manager";
 import Manager from "../components/Manager";
+import ReportTIme from "../components/ReportTIme";
+import AccountSettings from "../components/AccountSettings";
 
 Vue.use(Router)
 
@@ -11,9 +13,12 @@ export default new Router({
   mode:'history',
   routes: [
     { path: '/', name:'Home', component: Home },
+
     { path: '/users', name:'Users', component: Users },
+    { path: '/users/report-time', name: 'report-time', component: ReportTIme },
+    { path: '/users/account-settings', name: 'account-settings', component: AccountSettings },
+
     { path: '/general-manager', name: 'GeneralManager', component: General_Manager },
     { path: '/manager', name: 'Manager', component: Manager },
-    { path: '/manager/route', name: 'Manager', component: Manager }
   ]
 })
